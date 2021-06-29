7e6ee58ecb4c1b2d324c9b2463f99de86054cf6b
Divas Subedi <divas.subedi@gmail.com>
2021-06-29 15:56:55 -0400

    housekeeping: deleted duplicates

--- data-collection-src/switch.py
+++
@@ -1,9 +0,0 @@
-# servo control
-import os
-import time #library for sleep function
-import RPi.GPIO as GPIO #library for Raspberry Pi output pins
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.output(17,GPIO.HIGH)
-    
