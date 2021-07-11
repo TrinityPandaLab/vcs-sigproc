8c02c9fc36d3db3c0ccaa74399ac815c59026acd
TrinityPandaLab <trinitypandalab@gmail.com>
2021-07-11 12:25:16 -0400

    Update Codebase

--- freqatpeak.m
+++ freqatpeak.m
@@ -1,26 +1,15 @@
-function [zfreq, prominence] = freqatpeak(T, threshold)
+function [zfreq, prominence] = freqatpeak(obj, threshold)
 % Finds the peak frequency and it's promince between the threshold
     narginchk(1,2);
     
-    Fs = 3200;      % Sampling Frequency  
-    z = T(:,4);     % acc in z-direction
-    
-    % Getting rid of the offset
-    offset = mean(z);
-    z = z - offset;
-    freq = 0:Fs/length(z):Fs/2;
-   
-    % Calc Fourier Tranformation for Z-axis
-    N = length(z);
-    zdft = fft(z);
-    zdft = zdft(1:N/2+1);
-    zdft = abs(zdft);
-    
     if nargin == 2
-        [freq, zdft] = tools.cutoffat(freq,zdft,threshold, 2);
+        [freq, zdft] = obj.fftat(threshold);
+    else
+        freq = obj.freq;
+        zdft = obj.dft.z;
     end
     
-    [zmax, ind_zmax] = max(abs(zdft));
+    [zmax, ind_zmax] = max(zdft);
     zfreq = freq(ind_zmax);
     
 %     minimum = max([min(zdft(1:ind_zmax)), min(zdft(ind_zmax:end))]);
