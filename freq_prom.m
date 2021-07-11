73885eb20d23c6d4a6598ff3a765f84fa08f8e81
Divas Subedi <divas.subedi@gmail.com>
2021-07-11 16:15:44 -0400

    Update Codebase

--- freq_prom.m
+++ freq_prom.m
@@ -1,12 +1,12 @@
 function res = freq_prom(in_str, range)
     % Creates vectior if max_freq and the prominence
     d = dir(in_str);
+    
     for ii = 1:size(d,1)
-        filename = d(ii).name;
-        filename = strcat('Contact-Sensor-Data/' , filename);
-        obj = sigProc(filename, 800);
+        filename = d(ii).folder + "/" + d(ii).name;
+        obj = sigProc(filename, 3200);
         
-        [zfreq, prominence] = freqatpeak(obj, range);
+        [zfreq, prominence] = obj.freqatpeak(range);
 %         [zfreq, prominence] = freqatpeak(obj, range);
         res(ii,:) = [zfreq, prominence];
     end
--- tools.m
+++ tools.m
@@ -7,29 +7,44 @@ classdef tools
             output = (b.^(bHat(1)+ bHat(2).*z(:,1)+ bHat(3).*z(:,2)))./(1 + b.^(bHat(1) + bHat(2).*z(:,1)+ bHat(3).*z(:,2)));
         end
         
-        function [freq, zdft] = dft(sig, Fs)
-        %
+        function [freq, dft] = dft(sig, Fs)
+        % Static DFT function
             % Getting rid of the offset
             sig = sig - mean(sig);
 
             % Calc Fourier Tranformation
             N = length(sig);
-            zdft = abs(fft(sig));
-            zdft = zdft(1:N/2+1);
+            dft = abs(fft(sig));
+            dft = dft(1:N/2+1);
             freq = 0:Fs/length(sig):Fs/2;
 
             %Getting rid of freq 0
             freq = freq(1:end);
-            zdft = zdft(1:end);
+            dft = dft(1:end);
         end
         
-        function [freq, dft] = cutoffat(freq, dft, range|)
+        function [x1, x2] = cutoffat(x1, x2, range)
         % Splice the dft data for given frequencies
         % get dft for freq between of theresholds
-            index_1 = find(freq > range(1), 1);
-            index_2 = find(freq > range(2), 1);
-            freq = freq(index_1:index_2);
-            dft = dft(index_1:index_2);
+            index_1 = find(x1 > range(1), 1);
+            index_2 = find(x1 > range(2), 1);
+            x1 = x1(index_1:index_2);
+            x2 = x2(index_1:index_2);
+        end
+        
+        function [x1, prominence] = x1_atpeak_x2(x1, x2, range)
+        % Finds the x1 at peak x2 and it's promince between the range
+            narginchk(1,2);
+
+            if nargin == 2
+                [x1, x2] = tools.cutoff(x1,x2,range);
+            end
+
+            [x2max, ind_x2max] = max(x2);
+            x1 = x1(ind_x2max);
+
+            minimum = max(x2(1), x2(end));
+            prominence = x2max - minimum;
         end
     end
 end
